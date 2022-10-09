29aeaf8de3192051a562d02304931b29a73ec320
Brendan Losinski <88355167+blosinski@users.noreply.github.com>
2022-10-09 15:29:48 -0500

    Initial commit

---
+++ components/Main.jsx
@@ -0,0 +1,60 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
+const Main = () => {
+  return (
+    <div id="home" className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          {/* <p className="uppercase text-sm tracking-widest text-gray-600">
+            Let's build something together
+          </p> */}
+          <h1 className="py-4 text-gray-700">
+            Hey! I'm <span className="text-[#d43f3f]">Brendan</span>
+          </h1>
+          <h1 className="py-2 text-gray-400">A Mobile Developer</h1>
+          <p className="py-4 text-gray-600 max-w-[70%] m-auto">
+            As a software engineer specializing in building and designing
+            exceptional iOS Mobile Apps, I have a passion for creating
+            innovative and user-friendly applications. I am constantly striving
+            to improve my skillset and keep up-to-date on the latest trends in
+            mobile app development.
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <a
+              href="https://www.linkedin.com/in/brendan-losinski-60abb9208/"
+              target="_blank"
+            >
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <FaLinkedinIn />
+              </div>
+            </a>
+            <a
+              href="https://github.com/blosinski?tab=repositories"
+              target="_blank"
+            >
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <FaGithub />
+              </div>
+            </a>
+            <a href="mailto:brendan.losinski@yahoo.com">
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <AiOutlineMail />
+              </div>
+            </a>
+            <a href="Resume.pdf" target="_blank" download>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <BsFillPersonLinesFill />
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
---
+++ components/Projects.jsx
@@ -0,0 +1,63 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import calculatorImg from '../public/assets/projects/CalculatorAppPicture.png';
+import locationImg from '../public/assets/projects/LocationAppPicture.png';
+import stockImg from '../public/assets/projects/StockAppPicture.png';
+import uberImg from '../public/assets/projects/UberAppPicture.png';
+import uiImg from '../public/assets/projects/UISlideoutAppPicture.png';
+import walletImg from '../public/assets/projects/WalletAppPicture.png';
+import ProjectItem from './ProjectItem';
+
+const Projects = () => {
+  return (
+    <div id="projects" className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#d43f3f]">
+          Projects
+        </p>
+        <h2 className="py-4">What I&apos;ve Built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          <ProjectItem
+            title="Calculator"
+            backgroundImg={calculatorImg}
+            projectUrl="https://github.com/blosinski/CalculatorApp"
+            tech="SwiftUI"
+          />
+          <ProjectItem
+            title="Location Lookup"
+            backgroundImg={locationImg}
+            projectUrl="https://github.com/blosinski/Location-App"
+            tech="Swift"
+          />
+          <ProjectItem
+            title="Stock API Tracker"
+            backgroundImg={stockImg}
+            projectUrl="https://github.com/blosinski/Crypto-App"
+            tech="SwiftUI"
+          />
+          <ProjectItem
+            title="Uber 2.0"
+            backgroundImg={uberImg}
+            projectUrl="https://github.com/blosinski/Uber2.0"
+            tech="SwiftUI"
+          />
+          <ProjectItem
+            title="Twitter UI"
+            backgroundImg={uiImg}
+            projectUrl="https://github.com/blosinski/TwitterUI"
+            tech="SwiftUI"
+          />
+          <ProjectItem
+            title="Apple Wallet 2.0"
+            backgroundImg={walletImg}
+            projectUrl="https://github.com/blosinski/AppleWalletUI"
+            tech="SwiftUI"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
