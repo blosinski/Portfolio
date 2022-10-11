7ae3489d5f62a1f748265887d246fa5cbde139a1
Brendan Losinski <88355167+blosinski@users.noreply.github.com>
2022-10-11 11:09:21 -0500

    Update Skills.jsx

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -14,7 +14,7 @@ const Skills = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
-                  src="public/assets/skills/swift.png"
+                  src="./public/assets/skills/swift.png"
                   width="64px"
                   height="64px"
                   alt="/"
