b804e933578fd18812523c40b599ebe19bf6eb27
blosinski <brendan.losinski@yahoo.com>
2022-11-17 18:42:13 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,33 +8,6 @@ import Script from 'next/script'
 import React, { useState } from "react";
 
 export default function Home() {
-  const [query, setQuery] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    message: "",
-    subject: "",
-    'g-recaptcha-response': ""
-  });
-  const handleParam = () => (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setQuery((prevState) => ({
-      ...prevState,
-      [name]: value
-    }));
-  };
-  const formSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    Object.entries(query).forEach(([key, value]) => {
-      formData.append(key, value);
-    });
-    fetch("{https://getform.io/thank-you}", {
-      method: "POST",
-      body: formData
-    }).then(() => setQuery({ name: "", email: ""}));
-  };
   
   return (
     <div>
@@ -49,19 +22,6 @@ export default function Home() {
     <Skills />
     <Projects />
     <Contact />
-
-    <Script
-
-      src="https://www.google.com/recaptcha/api.js?render=6LegW9UiAAAAAOjncHpTnAWfJDULCGeAxINkUn5C"
-      onReady={() => {
-        grecaptcha.ready(function() {
-          grecaptcha.execute('6LegW9UiAAAAAOjncHpTnAWfJDULCGeAxINkUn5C', {action: 'homepage'})
-          .then(function(token) {
-            setQuery({'g-recaptcha-response': token})
-          });
-        });
-      }}
-    />
     
     </div>
     
